0d68a58017f88a73a9ad2ad0e67bce3baa37dc52
Clinton Halpin <clintonhalpin@gmail.com>
2014-10-12 12:32:59 -0400

    Add created_at & updated_at values to items model

--- api/components/item.model.js
+++ api/components/item.model.js
@@ -2,7 +2,17 @@ var mongoose = require('mongoose');
 
 var ItemSchema = new mongoose.Schema({
   name: String,
-  dateCreated: String 
+  created_at : { type: Date },
+  updated_at : { type: Date }
+});
+
+ItemSchema.pre('save', function(next){
+  now = new Date();
+  this.updated_at = now;
+  if ( !this.created_at ) {
+    this.created_at = now;
+  }
+  next();
 });
 
 module.exports = mongoose.model('Item', ItemSchema);
